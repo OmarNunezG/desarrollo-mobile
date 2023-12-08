5787f8d02f0bcf044f88d62f3897d53b55c1532f
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-12-08 12:41:51 -0400

    WOrked on labels

--- ejercicio_practico/lib/src/widgets/entry_labels.dart
+++ ejercicio_practico/lib/src/widgets/entry_labels.dart
@@ -10,24 +10,32 @@ class EntryLabels extends StatelessWidget {
     return Wrap(
       children: [
         Chip(
+          
+          avatar: Icon(Icons.wallet_giftcard),
           label: Text('Gift 1'),
         ),
         Chip(
+          avatar: Icon(Icons.wallet_giftcard),
           label: Text('Gift 2'),
         ),
         Chip(
+          avatar: Icon(Icons.wallet_giftcard),
           label: Text('Gift 3'),
         ),
         Chip(
+          avatar: Icon(Icons.wallet_giftcard),
           label: Text('Gift 4'),
         ),
         Chip(
+          avatar: Icon(Icons.wallet_giftcard),
           label: Text('Gift 5'),
         ),
         Chip(
+          avatar: Icon(Icons.wallet_giftcard),
           label: Text('Gift 6'),
         ),
         Chip(
+          avatar: Icon(Icons.wallet_giftcard),
           label: Text('Gift 7'),
         ),
       ],
